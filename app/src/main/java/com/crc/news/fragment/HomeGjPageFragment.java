1dc72aba3b0bc1f171f5b33518887d2d3eb62597
gongyuan <yuangong@163.com>
2017-04-18 20:51:47 +0800

    完成“首页”的央视新闻、一像素的分割线和首页标题。

---
+++ app/src/main/java/com/crc/news/fragment/HomeGjPageFragment.java
@@ -0,0 +1,30 @@
+package com.crc.news.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.crc.news.newsgy.R;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.x;
+
+/**
+ * Fragmet需要添加xml布局文件
+ */
+
+@ContentView(value = R.layout.home_gj_page)
+public class HomeGjPageFragment extends Fragment {
+
+
+    @Nullable
+    @Override  // 每个Fragment都有自己的XML配置文件
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return x.view().inject(this,inflater,null);
+    }
+
+
+}
--- app/src/main/java/com/crc/news/fragment/HomePageFragment.java
+++ app/src/main/java/com/crc/news/fragment/HomePageFragment.java
@@ -3,15 +3,24 @@ package com.crc.news.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.design.widget.TabLayout;
+
 
 import com.crc.news.newsgy.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Fragmet需要添加xml布局文件
@@ -20,13 +29,89 @@ import org.xutils.x;
 @ContentView(value = R.layout.home_page)
 public class HomePageFragment extends Fragment {
 
+    @ViewInject(R.id.tabHomeTitle)
+    private TabLayout tableHomeTitle;
+    @ViewInject(R.id.viewHomePager)
+    private ViewPager viewHomePager;
 
     @Nullable
     @Override  // 每个Fragment都有自己的XML配置文件
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.i("crc",this.getClass() + "---->2: onCreateView");
-        return x.view().inject(this,inflater,null);
+        View view =x.view().inject(this,inflater,null);
+        initData(view);
+        return view;
 //        return View.inflate(this.getActivity(), R.layout.home_page,null);
+    }
+
+
+    // 进行组件的赋值操作
+    private void initData(View view){
+
+//        tableLayout.setAdapter(new HomeTabFragmentPage(getSupportFragmentManager()));
+
+        HomeTabFragmentPage homeTabFragmentPage=new HomeTabFragmentPage(getActivity().getSupportFragmentManager());
+
+        //设置TabLayout的模式 setMode(TabLayout.MODE_FIXED)
+        tableHomeTitle.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tableHomeTitle.addTab(tableHomeTitle.newTab().setText(homeTabFragmentPage.getTitle(0)));
+        tableHomeTitle.addTab(tableHomeTitle.newTab().setText(homeTabFragmentPage.getTitle(1)));
+        tableHomeTitle.addTab(tableHomeTitle.newTab().setText(homeTabFragmentPage.getTitle(2)));
+        tableHomeTitle.addTab(tableHomeTitle.newTab().setText(homeTabFragmentPage.getTitle(3)));
+        tableHomeTitle.addTab(tableHomeTitle.newTab().setText(homeTabFragmentPage.getTitle(4)));
+        tableHomeTitle.addTab(tableHomeTitle.newTab().setText(homeTabFragmentPage.getTitle(5)));
+//        tableHomeTitle.addTab(tableHomeTitle.newTab().setText("要闻"));
+//        tableHomeTitle.addTab(tableHomeTitle.newTab().setText("直播"));
+//        tableHomeTitle.addTab(tableHomeTitle.newTab().setText("V观"));
+//        tableHomeTitle.addTab(tableHomeTitle.newTab().setText("联播"));
+//        tableHomeTitle.addTab(tableHomeTitle.newTab().setText("国际"));
+//        tableHomeTitle.addTab(tableHomeTitle.newTab().setText("体育"));
+
+        //viewpager加载adapter
+        viewHomePager.setAdapter(homeTabFragmentPage);
+        //TabLayout加载viewpager
+        tableHomeTitle.setupWithViewPager(viewHomePager);
+    }
+
+
+    private class HomeTabFragmentPage extends FragmentPagerAdapter {
+        private List<Fragment> listFragment=new ArrayList<Fragment>();                         //fragment列表
+        private List<String> listTitle=new ArrayList<String>();                              //tab名的列表
+
+        public HomeTabFragmentPage(FragmentManager fm) {
+            super(fm);
+            listFragment.add(new HomeYwPageFragment());
+            listFragment.add(new HomeZbPageFragment());
+            listFragment.add(new HomeVgPageFragment());
+            listFragment.add(new HomeLbPageFragment());
+            listFragment.add(new HomeGjPageFragment());
+            listFragment.add(new HomeTyPageFragment());
+
+            listTitle.add("要闻");
+            listTitle.add("直播");
+            listTitle.add("V观");
+            listTitle.add("联播");
+            listTitle.add("国际");
+            listTitle.add("体育");
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return listFragment.get(position);
+        }
+
+        public String getTitle(int position){ return listTitle.get(position);}
+
+        @Override
+        public int getCount() {
+            return listFragment.size();
+        }
+        //此方法用来显示tab上的名字
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return listTitle.get(position);
+        }
     }
 
 
